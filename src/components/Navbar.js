31a2d90fd9fb9cf9c92dfe3f735d58c599463479
thesoaprocks <aevum@MacBook-Pro-5.local>
2018-03-19 20:09:25 -0700

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,18 +22,7 @@ const Navbar = () => (
           Products
         </Link>
       </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
+      
     </div>
   </nav>
 )
